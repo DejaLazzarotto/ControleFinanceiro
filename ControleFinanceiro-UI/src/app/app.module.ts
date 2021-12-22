c1dc129d806a0ca9efbac4e24a0a3c1577438a24
Deja Lazzarotto <deja@deja.com.br>
2021-12-22 11:27:09 -0300

    Initial commit

---
+++ ControleFinanceiro-UI/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TiposService } from '../app/services/tipos.service';
+import { CategoriasService } from './services/categorias.service';
+import { ListagemCategoriasComponent } from './components/Categoria/listagem-categorias/listagem-categorias.component';
+import { NovaCategoriaComponent } from './components/Categoria/nova-categoria/nova-categoria.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { AtualizarCategoriaComponent } from './components/Categoria/atualizar-categoria/atualizar-categoria.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    ListagemCategoriasComponent, NovaCategoriaComponent, AtualizarCategoriaComponent
+  ],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    BrowserAnimationsModule, 
+    HttpClientModule,
+    MatTableModule, 
+    MatIconModule, 
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatGridListModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    TiposService, 
+    CategoriasService, 
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
